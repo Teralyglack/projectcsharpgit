f7bee5a4e43d7e795365bf759f7c5e63c7ff9f26
Teralyglack <tiffanimar005@gmail.com>
2025-05-24 22:29:12 +0300

    Начата разработка консольного интерфейса

---
+++ MyProjectCSharp/MyProjectCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Npgsql;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Строка подключения к вашей базе данных PostgreSQL
+        string mydatabase = "Host=localhost;Database=postgres;Username=postgres;Password=123";
+
+    }
+}
